382769164c5e41bd6a74d3b9a01daefb0955d9f6
Mate Matiovics <mate.matiovics@yahoo.com>
2024-02-18 16:56:27 +0100

    feat: add address service application

---
+++ addressservice/src/main/java/com/dontirikk/addressservice/web/AddressController.java
@@ -0,0 +1,23 @@
+package com.dontirikk.addressservice.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/address")
+public class AddressController {
+    public static final String ADDRESS = "Budapest, Komor Marcell u. 1, 1095";
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Address getAddress() {
+        return new Address(UUID.randomUUID(), ADDRESS);
+    }
+
+    public record Address(UUID id, String address) {}
+}
